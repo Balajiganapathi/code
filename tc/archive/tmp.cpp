73de6211fc6f3505b67755d955737a4184222fb4
Balajiganapathi S <balajiganapathi.s@gmail.com>
2016-08-13 15:37:01 +0530

    SRM problems

---
+++ tc/archive/tmp.cpp
@@ -0,0 +1,409 @@
+/* string coder = "Balajiganapathi S"; // Never give up!  */
+
+
+// BEGIN CUT HERE
+//#define LOCAL
+#ifdef LOCAL
+#   define TRACE
+#   define TEST
+#else
+#   define NDEBUG
+//#   define FAST
+#endif
+// END CUT HERE
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/* aliases */
+using vi  = vector<int>;
+using pi  = pair<int, int>;
+using ll  = long long int;
+
+/* shortcut macros */
+#define mp              make_pair
+#define fi              first
+#define se              second
+#define mt              make_tuple
+#define gt(t, i)        get<i>(t)
+#define all(x)          (x).begin(), (x).end()
+#define ini(a, v)       memset(a, v, sizeof(a))
+#define rep(i, s, n)    for(int i = (s), _##i = (n); i <= _##i; ++i)
+#define re(i, s, n)     rep(i, (s), (n) - 1)
+#define fo(i, n)        re(i, 0, n)
+#define si(x)           (int((x).size()))
+#define is1(mask,i)     (((mask) >> i) & 1)
+
+// BEGIN CUT HERE
+/* trace macro */
+#ifdef TRACE
+#   define trace(v...)  {cerr << __func__ << ":" << __LINE__ << ": " ;_dt(#v, v);}
+#else
+#   define trace(...)
+#endif
+
+#ifdef TRACE
+pi _gp(string s) {
+    pi r(0, si(s) - 1);
+    int p = 0, s1 = 0, s2 = 0, start = 1;
+    fo(i, si(s)) {
+        int x = (s1 | s2);
+        if(s[i] == ' ' && start) {
+            ++r.fi;
+        } else {
+            start = 0;
+            if(s[i] == ',' && !p && !x) {
+                r.se = i - 1;
+                return r;
+            }
+            if(x && s[i] == '\\') ++i;
+            else if(!x && s[i] == '(') ++p;
+            else if(!x && s[i] == ')') --p;
+            else if(!s2 && s[i] == '\'') s1 ^= 1;
+            else if(!s1 && s[i] == '"') s2 ^= 1;
+        }
+    }
+    return r;
+}
+
+template<typename H> void _dt(string u, H&& v) {
+    pi p = _gp(u);
+    cerr << u.substr(p.fi, p.se - p.fi + 1) << " = " << forward<H>(v) << " |" << endl;
+}
+
+template<typename H, typename ...T> void _dt(string u, H&& v, T&&... r) {
+    pi p = _gp(u);
+    cerr << u.substr(p.fi, p.se - p.fi + 1) << " = " << forward<H>(v) << " | ";
+    _dt(u.substr(p.se + 2), forward<T>(r)...);
+}
+
+template<typename T> 
+ostream &operator <<(ostream &o, vector<T> v) { // print a vector
+    o << "[";
+    fo(i, si(v) - 1) o << v[i] << ", ";
+    if(si(v)) o << v.back();
+    o << "]";
+    return o;
+}
+
+template<typename T1, typename T2> 
+ostream &operator <<(ostream &o, map<T1, T2> m) { // print a map
+    o << "{";
+    for(auto &p: m) {
+        o << " (" << p.fi << " -> " << p.se << ")";
+    }
+    o << " }";
+    return o;
+}
+
+template<typename T> 
+ostream &operator <<(ostream &o, set<T> s) { // print a set
+    o << "{";
+    bool first = true;
+    for(auto &entry: s) {
+        if(!first) o << ", ";
+         
+        o << entry;
+        first = false;
+    }
+    o << "}";
+    return o;
+}
+
+template <size_t n, typename... T>
+typename enable_if<(n >= sizeof...(T))>::type
+    print_tuple(ostream&, const tuple<T...>&) {} 
+
+template <size_t n, typename... T>
+typename enable_if<(n < sizeof...(T))>::type
+    print_tuple(ostream& os, const tuple<T...>& tup) {
+    if (n != 0)
+        os << ", ";
+    os << get<n>(tup);
+    print_tuple<n+1>(os, tup);
+}
+
+template <typename... T>
+ostream& operator<<(ostream& os, const tuple<T...>& tup) { // print a tuple
+    os << "("; print_tuple<0>(os, tup); return os << ")"; } template <typename T1, typename T2>
+ostream& operator<<(ostream& os, const pair<T1, T2>& p) { // print a pair
+    return os << "(" << p.fi << ", " << p.se << ")";
+}
+#endif
+// END CUT HERE
+    
+#define x1 _asdfzx1
+#define y1 _ysfdzy1
+
+/* constants */
+constexpr int dx[] = {-1, 0, 1, 0, 1, 1, -1, -1};
+constexpr int dy[] = {0, -1, 0, 1, 1, -1, 1, -1};
+constexpr auto PI  = 3.14159265358979323846L;
+constexpr auto oo  = numeric_limits<int>::max() / 2 - 2;
+constexpr auto eps = 1e-6;
+constexpr auto mod = 1000000007;
+
+/* code */
+constexpr int mx_b = 21;
+int dp[(1 << mx_b)];
+int cost[(1 << mx_b)];
+int vis[55];
+
+class Gperm {
+public:
+    map<pi, int> cnt;
+    vi v;
+    set<int> adj[55];
+    int rv[55];
+
+    void dfs(int x) {
+        if(vis[x]) return;
+        vis[x] = 1;
+        v.push_back(x);
+
+        for(auto nxt: adj[x]) {
+            dfs(nxt);
+        }
+    }
+
+    int solve() {
+        int n = si(v);
+        fo(i, n) rv[v[i]] = i;
+        int full = (1 << n) - 1;
+        dp[full] = 0;
+
+        rep(i, 0, full) {
+            cost[i] = 0;
+            fo(j, n) if(is1(i, j)) {
+                int x = v[j];
+                for(int y: adj[x])  if(is1(i, rv[y])) {
+                    int a = min(x, y), b = max(x, y);
+                    cost[i] += cnt[mp(a, b)];
+                }
+            }
+            cost[i] /= 2;
+        }
+
+        for(int i = full - 1; i >= 0; --i) {
+            dp[i] = oo;
+            fo(j, n) if(!is1(i, j)) {
+                int nxt = (i | (1 << j));
+                dp[i] = min(dp[i], cost[nxt] + dp[nxt]);
+            }
+        }
+
+        trace(v, dp[0]);
+        return dp[0];
+    }
+
+	int countfee( vector <int> x, vector <int> y ) {
+		int ret = oo;
+        fo(i, si(x)) {
+            if(x[i] > y[i]) swap(x[i], y[i]);
+            int a = min(x[i], y[i]);
+            int b = max(x[i], y[i]);
+            adj[a].insert(b);
+            adj[b].insert(a);
+            ++cnt[mp(a, b)];
+        }
+        ini(vis, 0);
+
+        int m = si(x);
+
+        fo(mask, (1 << m)) {
+            v.clear();
+            vi rem;
+            fo(i, m) {
+                if(is1(mask, i)) v.push_back(y[i]);
+                else v.push_back(x[i]);
+                rem.push_back(x[i] + y[i] - v.back());
+            }
+            int cur = solve();
+            vi remCosts;
+            for(int x: rem) {
+                for(int y: adj[x]) {
+                    int a = min(x, y);
+                    int b = max(x, y);
+                }
+            }
+            
+            ret = min(ret, cur);
+        }
+
+
+		
+		return ret;
+	}
+};
+
+// BEGIN CUT HERE
+namespace moj_harness {
+	int run_test_case(int);
+	void run_test(int casenum = -1, bool quiet = false) {
+		if (casenum != -1) {
+			if (run_test_case(casenum) == -1 && !quiet) {
+				cerr << "Illegal input! Test case " << casenum << " does not exist." << endl;
+			}
+			return;
+		}
+		
+		int correct = 0, total = 0;
+		for (int i=0;; ++i) {
+			int x = run_test_case(i);
+			if (x == -1) {
+				if (i >= 100) break;
+				continue;
+			}
+			correct += x;
+			++total;
+		}
+		
+		if (total == 0) {
+			cerr << "No test cases run." << endl;
+		} else if (correct < total) {
+			cerr << "Some cases FAILED (passed " << correct << " of " << total << ")." << endl;
+		} else {
+			cerr << "All " << total << " tests passed!" << endl;
+		}
+	}
+	
+	int verify_case(int casenum, const int &expected, const int &received, clock_t elapsed) { 
+		cerr << "Example " << casenum << "... "; 
+		
+		string verdict;
+		vector<string> info;
+		char buf[100];
+		
+		if (elapsed > CLOCKS_PER_SEC / 200) {
+			sprintf(buf, "time %.2fs", elapsed * (1.0/CLOCKS_PER_SEC));
+			info.push_back(buf);
+		}
+		
+		if (expected == received) {
+			verdict = "PASSED";
+		} else {
+			verdict = "FAILED";
+		}
+		
+		cerr << verdict;
+		if (!info.empty()) {
+			cerr << " (";
+			for (int i=0; i<(int)info.size(); ++i) {
+				if (i > 0) cerr << ", ";
+				cerr << info[i];
+			}
+			cerr << ")";
+		}
+		cerr << endl;
+		
+		if (verdict == "FAILED") {
+			cerr << "    Expected: " << expected << endl; 
+			cerr << "    Received: " << received << endl; 
+		}
+		
+		return verdict == "PASSED";
+	}
+
+	int run_test_case(int casenum__) {
+		switch (casenum__) {
+		case 0: {
+			int x[]                   = {0};
+			int y[]                   = {1};
+			int expected__            = 1;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 1: {
+			int x[]                   = {0,1};
+			int y[]                   = {1,2};
+			int expected__            = 2;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 2: {
+			int x[]                   = {4,7,7};
+			int y[]                   = {7,4,4};
+			int expected__            = 3;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 3: {
+			int x[]                   = {0,0,1};
+			int y[]                   = {1,2,2};
+			int expected__            = 4;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 4: {
+			int x[]                   = {7,8,9};
+			int y[]                   = {4,5,6};
+			int expected__            = 6;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+		case 5: {
+			int x[]                   = {45,28,42,5,27,27,42,36,14,27,19,42,24,27,8,31,24,27,14,28};
+			int y[]                   = {45,27,45,8,34,34,28,0,11,42,24,19,14,31,45,42,14,24,28,27};
+			int expected__            = 53;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}
+
+		// custom cases
+
+/*      case 6: {
+			int x[]                   = ;
+			int y[]                   = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 7: {
+			int x[]                   = ;
+			int y[]                   = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+/*      case 8: {
+			int x[]                   = ;
+			int y[]                   = ;
+			int expected__            = ;
+
+			clock_t start__           = clock();
+			int received__            = Gperm().countfee(vector <int>(x, x + (sizeof x / sizeof x[0])), vector <int>(y, y + (sizeof y / sizeof y[0])));
+			return verify_case(casenum__, expected__, received__, clock()-start__);
+		}*/
+		default:
+			return -1;
+		}
+	}
+}
+ 
+
+int main(int argc, char *argv[]) {
+	if (argc == 1) {
+		moj_harness::run_test();
+	} else {
+		for (int i=1; i<argc; ++i)
+			moj_harness::run_test(atoi(argv[i]));
+	}
+}
+// END CUT HERE
